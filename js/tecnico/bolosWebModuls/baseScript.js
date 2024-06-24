7442e043b99254edb55bbe474d25221e5a835de3
vicentlozano <lozanodroid@gmail.com>
2024-06-24 21:13:52 +0200

    juego terminado

--- js/tecnico/bolosWebModuls/baseScript.js
+++ js/tecnico/bolosWebModuls/baseScript.js
@@ -1,3 +1,60 @@
-import { tiradaPromise } from "./inicioTirada.mjs";
+import { tiradaPromise, asyncFunction, endGame } from "./inicioTirada.mjs";
 
-const result = await tiradaPromise;
+const button1 = document.querySelector(".buttonPlayer1");
+const button2 = document.querySelector(".buttonPlayer2");
+const number1 = document.querySelector(".resultPlayer1");
+const number2 = document.querySelector(".resultPlayer2");
+let numberThrow1 = 0;
+let numberThrow2 = 0;
+let throwResults1 = [];
+let throwResults2 = [];
+
+button1.addEventListener("click", async () => {
+  if (
+    !endGame(numberThrow1, numberThrow2, throwResults1, throwResults2) &&
+    numberThrow1 < 6 &&
+    numberThrow2 < 6
+  ) {
+    let aleatorio = await asyncFunction();
+    throwResults1.push(Number(aleatorio));
+    number1.textContent += aleatorio + "|";
+    number1.style.display = "";
+    numberThrow1++;
+  } else {
+    numberThrow1 = 0;
+    numberThrow2 = 0;
+    throwResults1 = [];
+    throwResults2 = [];
+    setTimeout(() => {
+      number2.textContent = "";
+      number1.textContent = "";
+      document.querySelector(".winPlayer1").textContent = "";
+      document.querySelector(".winPlayer2").textContent = "";
+    }, 5000);
+  }
+});
+
+button2.addEventListener("click", async () => {
+  if (
+    !endGame(numberThrow1, numberThrow2, throwResults1, throwResults2) &&
+    numberThrow1 < 6 &&
+    numberThrow2 < 6
+  ) {
+    let aleatorio = await asyncFunction();
+    throwResults2.push(Number(aleatorio));
+    number2.textContent += aleatorio + "|";
+    number2.style.display = "";
+    numberThrow2++;
+  } else {
+    numberThrow1 = 0;
+    numberThrow2 = 0;
+    throwResults1 = [];
+    throwResults2 = [];
+    setTimeout(() => {
+      number2.textContent = "";
+      number1.textContent = "";
+      document.querySelector(".winPlayer1").textContent = "";
+      document.querySelector(".winPlayer2").textContent = "";
+    }, 5000);
+  }
+});
--- js/tecnico/bolosWebModuls/inicioTirada.mjs
+++ js/tecnico/bolosWebModuls/inicioTirada.mjs
@@ -1,11 +1,45 @@
 export const tiradaPromise = () =>
-    new Promise((resolve, reject) => {
-      let numAleatorio = Math.floor(Math.random() * 10);
-      let tiempoTirada = Math.random() * 1000;
-      let error = "no has tirado bolos, error!";
-      if (numAleatorio) {
-        resolve({ numAleatorio, tiempoTirada });
-      } else {
-        setTimeout(() => reject({ error, tiempoTirada }), tiempoTirada);
-      }
-    });
+  new Promise((resolve, reject) => {
+    let numAleatorio = Math.floor(Math.random() * 10);
+    let tiempoTirada = Math.random() * 1000;
+    let error = "no has tirado bolos, error!";
+    if (numAleatorio >= 0) {
+      resolve({ numAleatorio, tiempoTirada });
+    } else {
+      setTimeout(() => reject({ error, tiempoTirada }), tiempoTirada);
+    }
+  });
+
+export const asyncFunction = async () => {
+  try {
+    const resultof = await tiradaPromise();
+    return resultof.numAleatorio.toString();
+  } catch (error) {
+    console.log(error.error, error.tiempoTirada);
+  }
+};
+
+export const endGame = (throws1, throws2, arrayP1, arrayP2) => {
+  if (throws1 == 5 && throws2 == 5) {
+    let winLost1 = 0;
+    let winLost2 = 0;
+    for (let element of arrayP1) {
+      winLost1 += element;
+    }
+    for (let element of arrayP2) {
+      winLost2 += element;
+    }
+    if (winLost1 == winLost2) {
+      document.querySelector(".winPlayer1").textContent = "TIE!";
+      document.querySelector(".winPlayer2").textContent = "TIE!";
+      return true;
+    } else {
+      winLost1 > winLost2
+        ? (document.querySelector(".winPlayer1").textContent = "PLAYER 1 WIN!")
+        : (document.querySelector(".winPlayer2").textContent = "PLAYER 2 WIN!");
+      return true;
+    }
+  } else {
+    return false;
+  }
+};
